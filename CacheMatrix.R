ed911323694b6c0894ba678187fc1bcb339ee318
Ajay Muppidi <ajayreddy.muppidi@gmail.com>
2015-07-26 09:11:30 +0530

    second assignment

---
+++ CacheMatrix.R
@@ -0,0 +1,60 @@
+# The golas of the functions below is to add a caching functionality to a matrix
+# They will create a matrix that allows this caching functionality and cache the inverse of that matrix
+# So when the matrix's inverse is required, it won't always be calculated, but returned from the cache if it was
+# previously calculated
+
+
+# This function accepts a matrix as its parameter and returns a new object that has functions to allow caching
+
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+
+# This function does the actually calculation of the inverse of the matrix, and saves it in "cache" for it to
+# be used again if requested
+
+cacheSolve <- function(x, ...) {
+  i <- x$getInverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setInverse(i)
+  # Returns the inverse of the matrix
+  i
+}
+
+## Sample run:
+## > x = rbind(c(1, -1/4), c(-1/4, 1))
+## > m = makeCacheMatrix(x)
+## > m$get()
+##       [,1]  [,2]
+## [1,]  1.00 -0.25
+## [2,] -0.25  1.00
+
+## No cache in the first run
+## > cacheSolve(m)
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+
+## Retrieving from the cache in the second run
+## > cacheSolve(m)
+## getting cached data.
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+## > 
